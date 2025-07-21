60aea91271842ce1d6bcf8981d6a82a4d3100841
Claire2311 <claire.girard@ieseg.fr>
2025-07-21 18:02:04 +0200

    task 3

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import '../widgets/text_field.dart';
+
+class SignUp extends StatefulWidget {
+  final TextEditingController emailController;
+  final TextEditingController usernameController;
+  final TextEditingController passwordController;
+  final TextEditingController passwordConfirmController;
+  final bool _passwordVisible = true;
+
+  const SignUp({
+    super.key,
+    required this.emailController,
+    required this.usernameController,
+    required this.passwordController,
+    required this.passwordConfirmController,
+  });
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  late bool _passwordVisible;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordConfirmController =
+      TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordVisible = widget._passwordVisible;
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _usernameController.dispose();
+    _passwordConfirmController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 100),
+            Text(
+              'Holbegram',
+              style: TextStyle(fontFamily: 'Billabong', fontSize: 50),
+            ),
+            Image.asset('assets/images/logo.webp', width: 80, height: 60),
+            Padding(
+              padding: EdgeInsets.all(30),
+              child: Text(
+                "Sign up to see photos and videos from your friends.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color.fromARGB(150, 83, 83, 83),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: <Widget>[
+                  TextFieldInput(
+                    controller: _emailController,
+                    ispassword: false,
+                    hintText: 'Email',
+                    suffixIcon: Icon(Icons.email),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  SizedBox(height: 24),
+                  TextFieldInput(
+                    controller: _usernameController,
+                    ispassword: false,
+                    hintText: 'Full Name',
+                    suffixIcon: Icon(Icons.person),
+                    keyboardType: TextInputType.text,
+                  ),
+                  SizedBox(height: 24),
+                  TextFieldInput(
+                    controller: _passwordController,
+                    ispassword: !_passwordVisible,
+                    hintText: 'Password',
+                    suffixIcon: IconButton(
+                      alignment: Alignment(-1.0, 1.0), //bottomLeft
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                      icon: Icon(
+                        _passwordVisible
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                      ),
+                    ),
+                    keyboardType: TextInputType.text,
+                  ),
+                  SizedBox(height: 24),
+                  TextFieldInput(
+                    controller: _passwordConfirmController,
+                    ispassword: !_passwordVisible,
+                    hintText: 'Confirm Password',
+                    suffixIcon: IconButton(
+                      alignment: Alignment(-1.0, 1.0), //bottomLeft
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                      icon: Icon(
+                        _passwordVisible
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                      ),
+                    ),
+                    keyboardType: TextInputType.text,
+                  ),
+                  SizedBox(height: 48),
+                  SizedBox(
+                    height: 48,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(
+                          Color.fromARGB(218, 226, 37, 24),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Divider(thickness: 2),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Have an account?"),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Log in",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(218, 226, 37, 24),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
